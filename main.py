22a49c526db672dd557b71b9c527e8b91e9be719
Lucas Dalmaso <79981704+07Dalmaso@users.noreply.github.com>
2023-03-17 13:09:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+pmt = float(input("Digite o valor mensal que deseja economizar: "))
+years = 10
+rate = 0.12
+n = years * 12
+fv = 0
+
+fv_list = []
+for i in range(1, years+1):
+    fv = (fv + pmt) * (1 + rate/12)**12
+    fv_list.append(fv)
+
+plt.bar(range(1, years+1), fv_list)
+plt.xlabel('Anos')
+plt.ylabel('Valor Futuro (R$)')
+plt.title('Evolução do valor ao longo de 10 anos')
+plt.show()
+print("O valor futuro é de R${:.2f}".format(fv))
